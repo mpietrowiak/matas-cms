75dcb89778d8eb8780d14eb7570f699835848041
Mateusz Pietrowiak <matas8@protonmail.com>
2020-04-16 01:39:17 +0200

    wi

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const InputElement = () => {
-  return <input placeholder="Enter Some Text" />;
+  const [inputText, setInputText] = useState('');
+
+  return <div><input 
+    onChange={(e) => {
+      setInputText(e.target.value)
+    }}
+    value={inputText}
+
+    placeholder="Enter Some Text" />
+    {inputText}
+  </div>
 };
 
 export default InputElement;
